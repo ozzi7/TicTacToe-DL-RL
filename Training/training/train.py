a4bdd27df0dddcf12ddcd4ad75a19352db60d980
Pascal Widmer <pawiddy@gmail.com>
2018-05-02 23:16:17 +0200

    init python train

---
+++ Training/training/train.py
@@ -0,0 +1,11 @@
+class Trainer:
+    def __init__(self):
+        self.NOF_FILTERS = 64
+        self.NOF_RESIDUAL_BLOCKS = 6
+
+    def train(self):
+        pass
+
+if __name__ == "__main__":
+    trainer = Trainer()
+    trainer.train()
